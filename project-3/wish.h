c16c4b10328dea771bdd5f104a6f92c6b682dc18
Steven Huynh <stevenhuynh70@yahoo.com>
2020-09-21 16:51:10 -0400

    Project 3 done

---
+++ project-3/wish.h
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <string.h>
+
+#define INTERACTIVE_MODE 1
+#define BATCH_MODE 2
+#define BUFF_SIZE 256
+
+struct function_args{
+    pthread_t thread;
+    char *command;
+};
+
+void printError(){
+    char error_message[30] = "An error has occurred\n";
+    write(STDERR_FILENO, error_message, strlen(error_message)); 
+}
+
+void clean(void);
+
+void * parseInput(void *arg);
+
+int searchPath(char path[], char *firstArg);
+
+void redirect(FILE *out);
+
+void executeCommands(char *args[], int args_num, FILE *out);
+
+char * trim(char *);
